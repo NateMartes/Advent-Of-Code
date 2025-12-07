4402136119dacf1fc37e1b0613fa9d32636b800f
Nathaniel Martes <nathaniel.martes.business@gmail.com>
2025-12-07 16:35:02 -0500

    Finish q4

---
+++ 2025/q4/question4.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"bufio"
+)
+
+type Point struct {
+    x int
+    y int
+}
+
+func readFile(name string) ([]string, error) {
+
+	content, err := os.Open(name)
+	if err != nil {
+		fmt.Printf("Error reading file %s: %v\n", name, err)
+		os.Exit(1)
+	}
+	defer content.Close()
+	var lines[]string
+	scanner := bufio.NewScanner(content)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func buildMatrix(rows []string) [][]string {
+
+	output := [][]string{}
+	for i := 0; i < len(rows); i++ {
+		curr := []string{}
+		for j := 0; j < len(rows[i]); j++ {
+			curr = append(curr, string(rows[i][j]))
+		}
+		output = append(output, curr)
+	}
+
+	return output
+}
+
+func inBound(x int, y int, xBound int, yBound int) bool {
+	if x < 0 || x >= xBound {
+		return false;
+	}
+
+	if y < 0 || y >= yBound {
+		return false;
+	}
+
+	return true;
+}
+
+func checkAdjacentPositions(matrix [][]string, x int, y int, xBound int, yBound int) bool {
+	count := 0;
+	roll := "@";
+	if (matrix[x][y] != roll) {
+		return false;
+	}
+	for i := x - 1; i <= x + 1; i++ {
+		for j := y - 1; j <= y + 1; j++ {
+			if !inBound(i, j, xBound, yBound) {
+				continue;
+			}
+			if i == x && j == y {
+				continue;
+			}
+			if (matrix[i][j] == roll) {
+				count++;
+			}
+		}
+	}
+
+	return count < 4;
+}
+
+func runThroughMatrix(matrix [][]string, seen map[Point]struct{}, x int, y int, xBound int, yBound int, count int) int {
+	if !inBound(x, y, xBound, yBound) {
+		return count;
+	}
+	_, exists := seen[Point{x: x, y: y}];
+	if exists {
+		return count;
+	}
+
+	seen[Point{x: x, y: y}] = struct{}{};
+	if checkAdjacentPositions(matrix, x, y, xBound, yBound) {
+		count++;
+	}
+
+	return max(runThroughMatrix(matrix, seen, x + 1, y, xBound, yBound, count),
+		   runThroughMatrix(matrix, seen, x - 1, y, xBound, yBound, count),
+		   runThroughMatrix(matrix, seen, x, y + 1, xBound, yBound, count),
+		   runThroughMatrix(matrix, seen, x, y - 1, xBound, yBound, count))
+}
+
+func main() {
+	file := "question4.input"
+	rows, _ := readFile(file)
+	matrix := buildMatrix(rows)
+	result := runThroughMatrix(matrix, make(map[Point]struct{}), 0, 0, len(matrix), len(matrix[0]), 0);
+	fmt.Println(result);
+}
